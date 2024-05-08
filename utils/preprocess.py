4b0a2524e892805904fab51996eb93cc642cc783
Guenter Bartsch <guenter@zamia.org>
2024-05-08 08:14:29 +0200

    add copyright notice

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,19 @@
 #!/bin/env python3
 
+'''
+zerovox
+
+    Apache 2.0 License
+    2024 by Guenter Bartsch
+
+is based on:
+
+    EfficientSpeech: An On-Device Text to Speech Model
+    https://ieeexplore.ieee.org/abstract/document/10094639
+    Rowel Atienza
+    Apache 2.0 License
+'''
+
 import argparse
 import scipy.signal
 import yaml
