95f31b8a5dfb4f0920e5b7ea95be967b1802b058
Guenter Bartsch <guenter@zamia.org>
2024-08-20 08:57:45 +0200

    adjust tb script

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #tensorboard --bind_all --logdir models/g2p_de_autoreg_zamia_1
-#tensorboard --bind_all --logdir models/tts_de_zerovox_medium_1
-tensorboard --bind_all --logdir models/tts_de_zerovox_base_1
+tensorboard --bind_all --logdir models/tts_de_zerovox_medium_2
+#tensorboard --bind_all --logdir models/tts_de_zerovox_base_3
 
 
