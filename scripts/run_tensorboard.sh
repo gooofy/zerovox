2da882d40145e8cc3c1af5539f71aa88217e988b
Guenter Bartsch <guenter@zamia.org>
2024-08-31 14:37:22 +0200

    small experiment

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -2,7 +2,8 @@
 
 #tensorboard --bind_all --logdir models/g2p_de_autoreg_zamia_1
 #tensorboard --bind_all --logdir models/tts_de_zerovox_medium_2
-tensorboard --bind_all --logdir models/tts_de_zerovox_medium_3
+#tensorboard --bind_all --logdir models/tts_de_zerovox_medium_3
+tensorboard --bind_all --logdir models/tts_de_zerovox_small_1
 #tensorboard --bind_all --logdir models/tts_de_zerovox_medium_karlsson_2
 #tensorboard --bind_all --logdir models/tts_de_zerovox_base_3
 
