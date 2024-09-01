addea93c605f1b261fc7bedae8ef5a96cb11c721
Guenter Bartsch <guenter@zamia.org>
2024-09-01 11:34:47 +0200

    update tensorboard launch script

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #tensorboard --bind_all --logdir models/g2p_de_autoreg_zamia_1
-#tensorboard --bind_all --logdir models/tts_de_zerovox_medium_2
-tensorboard --bind_all --logdir models/tts_de_zerovox_medium_3
-#tensorboard --bind_all --logdir models/tts_de_zerovox_medium_karlsson_2
-#tensorboard --bind_all --logdir models/tts_de_zerovox_base_3
 
+tensorboard --bind_all --logdir models/tts_de_zerovox_base_1
+#tensorboard --bind_all --logdir models/tts_de_zerovox_medium_3
+
+#tensorboard --bind_all --logdir models/tts_de_zerovox_medium_karlsson_2
 
