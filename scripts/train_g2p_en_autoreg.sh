f1a6bcd1e4e30455b507cba31673e5092414e465
Guenter Bartsch <guenter@zamia.org>
2024-09-24 22:11:14 +0200

    english g2p

---
+++ scripts/train_g2p_en_autoreg.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+python3 utils/train_g2p.py \
+            --accelerator=gpu \
+            --max_epochs=100 \
+            --warmup_epochs=10 \
+            --val_epochs=1 \
+            --lr=0.0001 \
+            --batch-size=384 \
+            --lang="en" \
+            --out-dir=models/g2p_en_autoreg_zamia_1 \
+            configs/g2p_en_autoreg_zamia.yaml
+
