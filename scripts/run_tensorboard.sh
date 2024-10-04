99f69b651f1972f5e3f93a0d8770d2c356de9227
Guenter Bartsch <guenter@zamia.org>
2024-10-04 10:04:20 +0200

    update tb launch script

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -7,4 +7,4 @@
 
 #tensorboard --bind_all --logdir models/tts_de_zerovox_medium_karlsson_2
 
-tensorboard --bind_all --logdir models/tts_de_zerovox_fs2dec_base_1
+tensorboard --bind_all --logdir models/tts_en_de_zerovox_base_1
