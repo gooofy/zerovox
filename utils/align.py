1524f62af143f3a4c348acfd2b06477ad4525553
Guenter Bartsch <guenter@zamia.org>
2024-12-26 11:01:02 +0100

    catch exceptions during alignment

--- utils/align.py
+++ utils/align.py
@@ -111,146 +111,151 @@ if __name__ == "__main__":
 
         sys.stdout.flush()
 
-        config = yaml.load(open(cfgfn, "r"), Loader=yaml.FullLoader)
+        try:
 
-        rawpath = Path(config['path']['raw_path'])
+            config = yaml.load(open(cfgfn, "r"), Loader=yaml.FullLoader)
 
-        workdir = Path(config['path']['preprocessed_path']) / 'work0'
-        print (workdir)
+            rawpath = Path(config['path']['raw_path'])
 
-        aligndir = Path(config['path']['preprocessed_path']) / 'align'
-        shutil.rmtree(aligndir, ignore_errors=True)
-        os.makedirs(aligndir, mode=0o755)
+            workdir = Path(config['path']['preprocessed_path']) / 'work0'
+            print (workdir)
 
-        shutil.rmtree(workdir, ignore_errors=True)
-        os.makedirs(workdir, mode=0o755)
+            aligndir = Path(config['path']['preprocessed_path']) / 'align'
+            shutil.rmtree(aligndir, ignore_errors=True)
+            os.makedirs(aligndir, mode=0o755)
 
-        os.symlink (kaldi_root / 'egs' / 'wsj' / 's5' / 'steps', workdir / 'steps')
-        os.symlink (kaldi_root / 'egs' / 'wsj' / 's5' / 'utils', workdir / 'utils')
-        os.symlink (kaldi_root / 'src', workdir / 'src')
+            shutil.rmtree(workdir, ignore_errors=True)
+            os.makedirs(workdir, mode=0o755)
 
-        with open (os.open(path=workdir / 'path.sh', flags=(os.O_WRONLY | os.O_CREAT  | os.O_TRUNC), mode=0o755), 'w') as pathf:
-            pathf.write("""
-    [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh\n
-    export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH\n
-    [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1\n
-    . $KALDI_ROOT/tools/config/common_path.sh\n
-    export LC_ALL=C\n
-    export PYTHONUNBUFFERED=1\n
-                    """)
+            os.symlink (kaldi_root / 'egs' / 'wsj' / 's5' / 'steps', workdir / 'steps')
+            os.symlink (kaldi_root / 'egs' / 'wsj' / 's5' / 'utils', workdir / 'utils')
+            os.symlink (kaldi_root / 'src', workdir / 'src')
 
-        os.makedirs(workdir / 'exp', mode=0o755)
-        os.makedirs(workdir / 'conf', mode=0o755)
-        os.makedirs(workdir / 'data' / 'alignme', mode=0o755)
-        os.makedirs(workdir / 'data' / 'lang', mode=0o755)
-        os.makedirs(workdir / 'data' / 'local' / 'lang', mode=0o755)
-        os.makedirs(workdir / 'lang', mode=0o755)
+            with open (os.open(path=workdir / 'path.sh', flags=(os.O_WRONLY | os.O_CREAT  | os.O_TRUNC), mode=0o755), 'w') as pathf:
+                pathf.write("""
+[ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh\n
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH\n
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1\n
+. $KALDI_ROOT/tools/config/common_path.sh\n
+export LC_ALL=C\n
+export PYTHONUNBUFFERED=1\n
+                        """)
 
-        lex = {}
-        oovs = set()
+            os.makedirs(workdir / 'exp', mode=0o755)
+            os.makedirs(workdir / 'conf', mode=0o755)
+            os.makedirs(workdir / 'data' / 'alignme', mode=0o755)
+            os.makedirs(workdir / 'data' / 'lang', mode=0o755)
+            os.makedirs(workdir / 'data' / 'local' / 'lang', mode=0o755)
+            os.makedirs(workdir / 'lang', mode=0o755)
 
-        cnt = 0
+            lex = {}
+            oovs = set()
 
-        with open(workdir / 'data' / 'alignme' / 'text', 'w') as textf:
-            with open(workdir / 'data' / 'alignme' / 'wav.scp', 'w') as wavscpf:
-                with open(workdir / 'data' / 'alignme' / 'utt2spk', 'w') as utt2spkf:
+            cnt = 0
 
-                    for labfn in os.listdir(rawpath):
+            with open(workdir / 'data' / 'alignme' / 'text', 'w') as textf:
+                with open(workdir / 'data' / 'alignme' / 'wav.scp', 'w') as wavscpf:
+                    with open(workdir / 'data' / 'alignme' / 'utt2spk', 'w') as utt2spkf:
 
-                        uttid, ext = os.path.splitext(labfn)
+                        for labfn in os.listdir(rawpath):
 
-                        if ext != '.lab':
-                            continue
+                            uttid, ext = os.path.splitext(labfn)
 
-                        with open (rawpath / labfn) as labf:
-                            text = labf.readline()
-
-                        tokens = tokenizer.tokenize(text)
-
-                        words = []
-                        for token in tokens:
-                            if re.search("[a-züöäß]", token) is None:
+                            if ext != '.lab':
                                 continue
-                            words.append(token)
 
-                            if (token not in lex) and (token not in oovs):
-                                if token in lexicon:
-                                    lex[token] = lexicon[token]
-                                else:
-                                    oovs.add(token)
+                            with open (rawpath / labfn) as labf:
+                                text = labf.readline()
 
-                        textf.write (f"{uttid} {' '.join(words)}\n")
+                            tokens = tokenizer.tokenize(text)
 
-                        wavscpf.write (f"{uttid} {str((rawpath / (uttid+'.wav')).resolve())}\n")
-                        utt2spkf.write (f"{uttid} 42\n")
+                            words = []
+                            for token in tokens:
+                                if re.search("[a-züöäß]", token) is None:
+                                    continue
+                                words.append(token)
 
-                        cnt += 1
+                                if (token not in lex) and (token not in oovs):
+                                    if token in lexicon:
+                                        lex[token] = lexicon[token]
+                                    else:
+                                        oovs.add(token)
 
-        num_jobs = min(args.num_jobs, cnt)
+                            textf.write (f"{uttid} {' '.join(words)}\n")
 
-        if oovs:
-            print (f"*** ERROR: {len(oovs)} OOVs found - use oovtool to generate pronounciations first.")
-            sys.exit(1)
+                            wavscpf.write (f"{uttid} {str((rawpath / (uttid+'.wav')).resolve())}\n")
+                            utt2spkf.write (f"{uttid} 42\n")
 
-        with open(workdir / 'conf' / 'mfcc.conf', 'w') as mfccf:
-            mfccf.write(f'--use-energy=false\n--sample-frequency={sampling_rate}\n')
+                            cnt += 1
 
-        ## for fn in ['lexicon.txt', 'lexiconp.txt', 'nonsilence_phones.txt', 'silence_phones.txt', 'optional_silence.txt']:
-        ##     shutil.copyfile(Path('models') / args.kaldi_model / 'work' / 'data' / 'local' / 'lang' / fn, workdir / 'data' / 'local' / 'lang' / fn)
+            num_jobs = min(args.num_jobs, cnt)
 
-        # phoneset = set()
+            if oovs:
+                print (f"*** ERROR: {len(oovs)} OOVs found - use oovtool to generate pronounciations first.")
+                sys.exit(1)
 
-        # with open( workdir / 'data' / 'local' / 'lang' / 'lexicon.txt', 'w') as lexf:
+            with open(workdir / 'conf' / 'mfcc.conf', 'w') as mfccf:
+                mfccf.write(f'--use-energy=false\n--sample-frequency={sampling_rate}\n')
 
-        #     lexf.write('<oov> spn\n')
-        #     for word in sorted(lex):
-        #         phones = lex[word]
-        #         lexf.write(f"{word} {' '.join(phones)}\n")
-        #         for p in phones:
-        #             phoneset.add(p)
+            ## for fn in ['lexicon.txt', 'lexiconp.txt', 'nonsilence_phones.txt', 'silence_phones.txt', 'optional_silence.txt']:
+            ##     shutil.copyfile(Path('models') / args.kaldi_model / 'work' / 'data' / 'local' / 'lang' / fn, workdir / 'data' / 'local' / 'lang' / fn)
 
-        # with open(workdir / 'data' / 'local' / 'lang' / 'nonsilence_phones.txt', 'w') as nspf:
-        #     for p in sorted(lexicon.phonemes):
-        #         nspf.write(f"{p}\n")
+            # phoneset = set()
 
-        # with open(workdir / 'data' / 'local' / 'lang' / 'silence_phones.txt', 'w') as spf:
-        #     spf.write(f"sil\nspn\n")
+            # with open( workdir / 'data' / 'local' / 'lang' / 'lexicon.txt', 'w') as lexf:
 
-        # with open(workdir / 'data' / 'local' / 'lang' / 'optional_silence.txt', 'w') as osf:
-        #     osf.write(f"sil\n")
+            #     lexf.write('<oov> spn\n')
+            #     for word in sorted(lex):
+            #         phones = lex[word]
+            #         lexf.write(f"{word} {' '.join(phones)}\n")
+            #         for p in phones:
+            #             phoneset.add(p)
 
-        # do_cmd( ['utils/prepare_lang.sh', '--position-dependent-phones', 'false', 'data/local/lang', '<oov>', 'data/local/', 'data/lang'], workdir)
-        do_cmd( ['utils/fix_data_dir.sh', 'data/alignme'], workdir )
-        do_cmd( ['steps/make_mfcc.sh', '--cmd', 'run.pl', '--nj', str(num_jobs), 'data/alignme', 'exp/make_mfcc/alignme', 'mfcc'], workdir)
-        # do_cmd( ['utils/fix_data_dir.sh', 'data/alignme'], workdir)
-        do_cmd( ['steps/compute_cmvn_stats.sh', 'data/alignme', 'exp/make_mfcc/data/alignme', 'mfcc'], workdir)
-        # do_cmd( ['utils/fix_data_dir.sh', 'data/alignme'], workdir)
+            # with open(workdir / 'data' / 'local' / 'lang' / 'nonsilence_phones.txt', 'w') as nspf:
+            #     for p in sorted(lexicon.phonemes):
+            #         nspf.write(f"{p}\n")
 
-        # do_cmd( ['steps/align_si.sh', '--nj', '1', '--cmd', 'run.pl', 'data/alignme', 'data/lang', str((kaldi_model_path / 'exp' / 'tri4a').resolve()), 'exp/tri4a_alignme'], workdir)
-        do_cmd( ['steps/align_si.sh', '--nj', '1', '--cmd', 'run.pl', 'data/alignme', str( (kaldi_model_path / 'data' / 'lang').resolve() ), str((kaldi_model_path / 'exp' / 'tri4a').resolve()), 'exp/tri4a_alignme'], workdir)
-        do_cmd( ['src/bin/ali-to-phones', '--ctm-output', str( (kaldi_model_path / 'exp' / 'tri4a' / 'final.mdl').resolve() ), 'ark:gunzip -c exp/tri4a_alignme/ali.1.gz|', 'foo.ctm'], workdir)
+            # with open(workdir / 'data' / 'local' / 'lang' / 'silence_phones.txt', 'w') as spf:
+            #     spf.write(f"sil\nspn\n")
 
-        with open (workdir / 'foo.ctm') as ctmf:
-            for line in ctmf:
-                #print(line)
+            # with open(workdir / 'data' / 'local' / 'lang' / 'optional_silence.txt', 'w') as osf:
+            #     osf.write(f"sil\n")
 
-                parts = line.strip().split(' ')
-                assert len(parts)==5
+            # do_cmd( ['utils/prepare_lang.sh', '--position-dependent-phones', 'false', 'data/local/lang', '<oov>', 'data/local/', 'data/lang'], workdir)
+            do_cmd( ['utils/fix_data_dir.sh', 'data/alignme'], workdir )
+            do_cmd( ['steps/make_mfcc.sh', '--cmd', 'run.pl', '--nj', str(num_jobs), 'data/alignme', 'exp/make_mfcc/alignme', 'mfcc'], workdir)
+            # do_cmd( ['utils/fix_data_dir.sh', 'data/alignme'], workdir)
+            do_cmd( ['steps/compute_cmvn_stats.sh', 'data/alignme', 'exp/make_mfcc/data/alignme', 'mfcc'], workdir)
+            # do_cmd( ['utils/fix_data_dir.sh', 'data/alignme'], workdir)
 
-                uttid = parts[0]
-                start = float(parts[2])
-                stop = float(parts[3])
-                pid = int(parts[4])
+            # do_cmd( ['steps/align_si.sh', '--nj', '1', '--cmd', 'run.pl', 'data/alignme', 'data/lang', str((kaldi_model_path / 'exp' / 'tri4a').resolve()), 'exp/tri4a_alignme'], workdir)
+            do_cmd( ['steps/align_si.sh', '--nj', '1', '--cmd', 'run.pl', 'data/alignme', str( (kaldi_model_path / 'data' / 'lang').resolve() ), str((kaldi_model_path / 'exp' / 'tri4a').resolve()), 'exp/tri4a_alignme'], workdir)
+            do_cmd( ['src/bin/ali-to-phones', '--ctm-output', str( (kaldi_model_path / 'exp' / 'tri4a' / 'final.mdl').resolve() ), 'ark:gunzip -c exp/tri4a_alignme/ali.1.gz|', 'foo.ctm'], workdir)
 
-                phone = phonemap[pid]
+            with open (workdir / 'foo.ctm') as ctmf:
+                for line in ctmf:
+                    #print(line)
 
-                l = f"{start}\t{stop}\t{phone}"
-                # print(l)
+                    parts = line.strip().split(' ')
+                    assert len(parts)==5
 
-                with open (aligndir / (uttid + '.tsv'), 'a') as alignf:
-                    alignf.write(l + '\n')
+                    uttid = parts[0]
+                    start = float(parts[2])
+                    stop = float(parts[3])
+                    pid = int(parts[4])
 
-        print()
+                    phone = phonemap[pid]
 
-        sys.stdout.flush()
+                    l = f"{start}\t{stop}\t{phone}"
+                    # print(l)
+
+                    with open (aligndir / (uttid + '.tsv'), 'a') as alignf:
+                        alignf.write(l + '\n')
+
+            print()
+
+            sys.stdout.flush()
+        except Exception as e:
+            print(f"*** ERROR: {cfgfn}: exception caught: {e}")
+
         
