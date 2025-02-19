7496e57bc64c9497eadbbcd52e3eaaef5ceef2ea
Guenter Bartsch <guenter@zamia.org>
2025-02-19 12:07:37 +0100

    adapt to latest model

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-tensorboard --bind_all --logdir models/tts_de_zerovox2_medium_1_styledec
+tensorboard --bind_all --logdir models/tts_de_zerovox2_medium_2_styledec
+# tensorboard --host 0.0.0.0 --logdir models/tts_de_zerovox2_medium_2_styledec
 
