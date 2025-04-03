369e213f7909dad85b72f176491305d610ea7f91
Guenter Bartsch <guenter@zamia.org>
2025-04-03 08:01:26 +0200

    update tf launch script

--- scripts/run_tensorboard.sh
+++ scripts/run_tensorboard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-tensorboard --bind_all --logdir models/tts_de_zerovox2_medium_4_styledec
+tensorboard --bind_all --logdir models/tts_de_zerovox2_medium_4
 # tensorboard --host 0.0.0.0 --logdir models/tts_de_zerovox2_medium_2_styledec
 
